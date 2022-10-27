c0fdab43288868799c0f3a406639377a34471b15
rimonspi12 <rimonmpi@gmail.com>
2022-10-27 22:43:11 +0600

    sample doctor page show

--- src/Pages/Home/SampleDoctor/SampleDoctor.js
+++ src/Pages/Home/SampleDoctor/SampleDoctor.js
@@ -5,24 +5,7 @@ import PrimaryButton from '../../Shared/PrimaryButton/PrimaryButton';
 
 const SampleDoctor = () => {
     return (
-        // <section
-        //     style={{
-        //         background: `url(${bgImg})`
-        //     }}
-        // >
-        //     <div className="hero">
-        //         <div className="hero-content flex-col lg:flex-row">
-        //             <img src={doctor} alt='not found' className="mt-[-100px] max-w-sm rounded-lg" />
-        //             <div>
-        //                 <h1 className="text-5xl font-bold">Box Office News!</h1>
-        //                 <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        //                 <button className="btn btn-primary">Get Started</button>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </section>
-
-        <section className='flex justify-center items-center mb-6 text-white'
+        <section className='flex justify-center items-center sm:py-12 mb-5 text-white'
             style={{
                 background: `url(${bgImg})`,
                 backgroundPosition: 'center center',
@@ -30,22 +13,19 @@ const SampleDoctor = () => {
                 backgroundAttachment: 'fixed'
 
             }}>
-            <div className="flex-1">
-                <figure><img src={doctor} className="mt-[-100px]" alt="Shoes" /></figure>
+            <div className="flex-1 hidden lg:block">
+                <figure><img src={doctor} className="mt-[-120px] " alt="Shoes" /></figure>
             </div>
-            <div className="flex-1">
-                <h3 className='text-primary text-3xl '>Appointment</h3>
-                <h2 className='text-4xl py-7'>Make an appointment</h2>
-                <p>Lorem ipsum dolor sit amet consectetur
+            <div className="flex-1 px-5">
+                <h3 className='text-primary text-3xl'>Appointment</h3>
+                <h2 className='text-4xl py-4'>Make an appointment</h2>
+                <p className='mb-5'>Lorem ipsum dolor sit amet consectetur
                     adipisicing elit. Nostrum, sit officia saepe r
                     epellat sapiente, eaque facilis eos nobis corrupti
                     veritatis est, et aliquam hic repellendus quam reiciendis?
                     Fugit neque deleniti necessitatibus repellendus soluta beatae ex
                     inventore sit ullam. Expedita, numquam.</p>
-
-                <div className='mt-5'>
-                    <PrimaryButton>Get started</PrimaryButton>
-                </div>
+                <PrimaryButton>Get started</PrimaryButton>
 
             </div>
         </section>
