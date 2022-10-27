fce29d722e957e41fb695170ed19e955f6235af1
rimonspi12 <rimonmpi@gmail.com>
2022-10-27 19:11:10 +0600

    navbar part added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Pages/Home/Navbar/Navbar';
+import Home from './Pages/Home/Home/Home';
+
 
 function App() {
-  return (
-	  <div>
-		  <button className="btn btn-active btn-secondary">Button</button>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
