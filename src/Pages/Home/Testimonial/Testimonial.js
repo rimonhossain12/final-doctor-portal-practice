cd30d89e19b521751b2d436bc8655f0f2362c29e
rimonspi12 <rimonmpi@gmail.com>
2022-10-28 16:01:23 +0600

    one picture remove

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -3,7 +3,6 @@ import quote from '../../../assets/icons/quote.svg';
 import Review from '../Review/Review';
 import people1 from '../../../assets/images/people1.png';
 import people2 from '../../../assets/images/people2.png';
-import people3 from '../../../assets/images/people3.png';
 
 const Testimonial = () => {
     const review = [
